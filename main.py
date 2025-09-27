c37bb2c784a01f45f141ed372bb2e6f65e7fc80f
nemihail <nemihail02@gmail.com>
2025-09-27 22:19:25 +0700

    Add files via upload
    полное соответствие техническому заданию, уже готово к к сдаче

--- main.py
+++ main.py
@@ -1,45 +1,29 @@
 
 book_list = []
 
-def check(finding):
-    for elem in book_list:
-        if finding = elem.lower():
-            return True
-        else:
-            pass
-
 while True:
     print()
-    for elem in book_list:
-        print(elem)
-
-    print('\nЧто сделать нужно с книгой?')
+    for elem, i in zip(book_list, range(len(book_list))):
+        print(i, '. ', elem)
+    print('\nЧто сделать со списком?')
     input_command = input('Добавить/Удалить/Индекс/Психануть: ')
-    if input_command.lower() == 'добавить':
-        book_list.append(input('Введи название книги: '))
-    elif input_command.lower() == 'удалить':
-        input_command = input('Введи название книги: ').lower()
-        if check(input_command) == True:
-            book_list.remove(input_command)
-        else:
-            print('Такой книги не найдено')
-    elif input_command.lower() == 'индекс':
-        input_command == int(input('Введи индекс: '))
+    if input_command.strip().lower() == 'добавить':
+        input_command = input('Введи имя книги: ')
+        book_list.append(input_command)
+    elif input_command.strip().lower() == 'удалить':
+        input_command = int(input('Введите индекс книги: '))
         if input_command < len(book_list):
-            if check(book_list[input_command]) == True:
-                print(book_list[input_command])
-            else:
-                print('Такой книги не найдено')
+            book_list.pop(input_command)
         else:
-            print('В списке нет столько книг!')
-            
-    elif input_command.lower() == 'психануть':
-        if input('Точно? Да/Нет: ').lower() == 'да':
-              book_list.clear()
-        elif input('Точно? Да/Нет: ').lower() == 'нет':
-              print('Правильно, читать полезно')
+            print('\nНет столько книг!')
+    elif input_command.strip().lower() == 'индекс':
+        input_command = int(input('Введите индекс книги: '))
+        if input_command < len(book_list):
+            print('\n', book_list[input_command])
         else:
-              print('Не понял тебя')
+            print('\nНет столько книг!')
+    elif input_command.strip().lower() == 'психануть':
+        book_list.clear()
     else:
-        print('Не понял тебя')
-        
+        print('\nНе поняи тебя')
+    
